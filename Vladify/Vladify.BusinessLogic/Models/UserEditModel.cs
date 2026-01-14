0a4606f3124c45e9fc83b3ba48df42b9a7271676
Vladislav Pomozov <vladuk2332@gmail.com>
2026-01-14 14:34:57 +0300

    added models

---
+++ Vladify/Vladify.BusinessLogic/Models/UserEditModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vladify.DataAccess.Enums;
+
+namespace Vladify.BusinessLogic.Models
+{
+    public class UserEditModel
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; } = 0;
+        public Gender Gender { get; set; } = Gender.Male;
+
+    }
+}
