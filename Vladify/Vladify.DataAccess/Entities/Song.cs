0be3fa4d4b6a1ef2475533fbd7c9a3a2d592970b
Vladislav Pomozov <vladuk2332@gmail.com>
2026-01-14 15:55:21 +0300

    another cosmetic fix

--- Vladify/Vladify.DataAccess/Entities/Song.cs
+++ Vladify/Vladify.DataAccess/Entities/Song.cs
@@ -6,5 +6,5 @@ public class Song
     public string Title { get; set; } = string.Empty;
     public string Album { get; set; } = string.Empty;
     public string Author { get; set; } = string.Empty;
-    public TimeSpan Duration { get; set; } = TimeSpan.FromMinutes(3);
+    public TimeSpan Duration { get; set; } = TimeSpan.Zero;
 }
