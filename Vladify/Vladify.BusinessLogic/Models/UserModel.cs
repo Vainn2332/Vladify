3fb01700388e575202e4f71415080741e50b38be
Vladislav Pomozov <vladuk2332@gmail.com>
2026-01-15 11:22:34 +0300

    Feature/add models (#2)
    * added User & Song Entities
    
    * added user model
    
    * added models
    
    * minor fixes in models
    
    * minior fixes 2
    
    * another fixes
    
    * some additional cosmetic fixes
    
    * another cosmetic fix
    
    * another fix
    
    * certainly final fix
    
    * absolutely last fix
    
    * edited models
    
    * edited user

---
+++ Vladify/Vladify.BusinessLogic/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vladify.DataAccess.Enums;
+
+namespace Vladify.BusinessLogic.Models
+{
+    internal class UserModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string EmailAddress { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public required int Age { get; set; }
+
+        public Gender Gender { get; set; } = Gender.Undefined;
+    }
+}
---
+++ Vladify/Vladify.BusinessLogic/Models/UserRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using Vladify.DataAccess.Enums;
+
+namespace Vladify.BusinessLogic.Models;
+
+public class UserRequestModel
+{
+    public required string Name { get; set; }
+
+    public required string EmailAddress { get; set; }
+
+    public required string Password { get; set; }
+
+    public required int Age { get; set; }
+
+    public required Gender Gender { get; set; }
+}
