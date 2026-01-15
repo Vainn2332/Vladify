ddd351134a8cd5541756a39711bfbf2fe43d95f5
Vladislav Pomozov <vladuk2332@gmail.com>
2026-01-15 13:23:00 +0300

    minor changes

--- Vladify/Vladify.DataAccess/UserDbConfig.cs
+++ Vladify/Vladify.DataAccess/UserDbConfig.cs
@@ -1,6 +1,16 @@
-﻿namespace Vladify.DataAccess
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vladify.DataAccess.Entities;
+
+namespace Vladify.DataAccess;
+
+public class UserDbConfig : IEntityTypeConfiguration<User>
 {
-    public class UserDbConfig
+    public void Configure(EntityTypeBuilder<User> builder)
     {
+        builder.HasKey(p => p.Id);
+        builder.HasIndex(p => p.EmailAddress).IsUnique();
+        builder.Property(p => p.Gender).HasConversion<string>();//сохраняем Male вместо 1 в БД
+        builder.Property(p => p.Name).HasMaxLength(50);
     }
 }
