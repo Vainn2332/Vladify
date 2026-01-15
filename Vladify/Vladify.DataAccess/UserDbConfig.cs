87ec2b023b512cfc1c6209c85de99ad2c75f75b4
Vladislav Pomozov <vladuk2332@gmail.com>
2026-01-15 16:40:50 +0300

    Update UserDbConfig.cs
    minor changes

--- Vladify/Vladify.DataAccess/UserDbConfig.cs
+++ Vladify/Vladify.DataAccess/UserDbConfig.cs
@@ -11,7 +11,7 @@ public class UserDbConfig : IEntityTypeConfiguration<User>
     {
         builder.HasKey(p => p.Id);
         builder.HasIndex(p => p.EmailAddress).IsUnique();
-        builder.Property(p => p.Gender).HasConversion<string>();//сохраняем Male вместо 1 в БД
+        builder.Property(p => p.Gender).HasConversion<string>();//saving Male instead of 1 in Db
         builder.Property(p => p.Name).HasMaxLength(DALConstants.MaxStandartStringLength);
     }
 }
