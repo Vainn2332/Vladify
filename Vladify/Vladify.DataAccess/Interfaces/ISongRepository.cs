e88a879d36b96d99d74dcd934dc31382ff2fc2ad
Vladislav Pomozov <vladuk2332@gmail.com>
2026-01-19 13:06:47 +0300

    added SongRepository Interface

---
+++ Vladify/Vladify.DataAccess/Interfaces/ISongRepository.cs
@@ -0,0 +1,12 @@
+﻿using Vladify.DataAccess.Entities;
+
+namespace Vladify.DataAccess.Interfaces;
+
+public interface ISongRepository
+{
+    public Task<Song?> GetByIdAsync(Guid songId);
+    public Task<IEnumerable<Song>> GetAsync();
+    public Task AddAsync(Song song);
+    public Task UpdateAsync(Guid oldSongId, Song newSong);
+    public Task DeleteAsync(Guid songId);
+}
