551c107a1fbc4d011bd3aa0e65824b77b98984d8
Vladislav Pomozov <vladuk2332@gmail.com>
2026-01-21 11:10:18 +0300

    another minor changes

--- Vladify/Vladify.BusinessLogic/ServiceInterfaces/ISongService.cs
+++ Vladify/Vladify.BusinessLogic/ServiceInterfaces/ISongService.cs
@@ -6,7 +6,7 @@ public interface ISongService
 {
     public Task<SongModel?> GetSongByIdAsync(Guid songId);
     public Task<IEnumerable<SongModel>> GetSongsAsync();
-    public Task<SongModel> AddSongAsync(SongRequestModel songModel);
+    public Task<SongModel> AddSongAsync(SongRequestModel songRequestModel);
     public Task<SongModel> UpdateSongAsync(SongModel SongModel);
     public Task DeleteSongAsync(Guid songId);
 }
