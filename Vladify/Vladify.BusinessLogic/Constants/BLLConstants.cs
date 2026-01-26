0f1528d7afd2e5470d86b6e2a163e49693947299
Vladislav Pomozov <vladuk2332@gmail.com>
2026-01-26 14:42:50 +0300

    added songValidator

--- Vladify/Vladify.BusinessLogic/Constants/BLLConstants.cs
+++ Vladify/Vladify.BusinessLogic/Constants/BLLConstants.cs
@@ -5,4 +5,11 @@ public static class BLLConstants
     public const int DefaultPaginationPageNumber = 1;
 
     public const int DefaultPaginationPageSize = 20;
+
+    public const int MaxDefaultStringLength = 50;
+
+    public static readonly TimeSpan MinSongDuration = TimeSpan.FromMinutes(2);
+
+    public static readonly TimeSpan MaxSongDuration = TimeSpan.FromMinutes(5);
+
 }
---
+++ Vladify/Vladify.BusinessLogic/Validators/SongValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Vladify.BusinessLogic.Constants;
+using Vladify.BusinessLogic.Models;
+
+namespace Vladify.BusinessLogic.Validators;
+
+public class SongValidator : AbstractValidator<SongRequestModel>
+{
+    public SongValidator()
+    {
+        RuleFor(song => song.Title)
+            .NotEmpty().WithMessage("Field 'Title' is required!")
+            .MaximumLength(BLLConstants.MaxDefaultStringLength).WithMessage("The length of field 'Title' exceeds maximum!");
+
+        RuleFor(song => song.Album)
+            .NotEmpty().WithMessage("Field 'Album' is required!")
+            .MaximumLength(BLLConstants.MaxDefaultStringLength).WithMessage("The length of field 'Album' exceeds maximum!");
+
+        RuleFor(song => song.Author)
+            .NotEmpty().WithMessage("Field 'Author' is required!")
+            .MaximumLength(BLLConstants.MaxDefaultStringLength).WithMessage("The length of field 'Author' exceeds maximum!");
+
+        RuleFor(song => song.Duration)
+            .NotEmpty().WithMessage("Field 'Duration' is required!")
+            .InclusiveBetween(BLLConstants.MinSongDuration, BLLConstants.MaxSongDuration).WithMessage("Song length must be between 2 and 5 minutes"!);
+    }
+}
