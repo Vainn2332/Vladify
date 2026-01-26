1f310a1901946d468f329435336e46f1742584d8
Vladislav Pomozov <vladuk2332@gmail.com>
2026-01-26 14:43:25 +0300

    added validation action filter

---
+++ Vladify/Vladify/Filters/ValidationFilter.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Vladify.BusinessLogic.Exceptions;
+
+namespace Vladify.Filters;
+
+public class ValidationFilter : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        foreach (var argument in context.ActionArguments.Values)
+        {
+            if (argument is null) continue;
+
+            var validatorType = typeof(IValidator<>).MakeGenericType(argument.GetType());
+            var validator = context.HttpContext.RequestServices.GetService(validatorType) as IValidator;
+
+            if (validator is not null)
+            {
+                var validationContext = new ValidationContext<object>(argument);
+                var validationResult = await validator.ValidateAsync(validationContext);
+
+                if (!validationResult.IsValid)
+                {
+                    throw new ValidationFailedException(validationResult.Errors[0].ErrorMessage);
+                }
+            }
+        }
+
+        await next();
+    }
+}
