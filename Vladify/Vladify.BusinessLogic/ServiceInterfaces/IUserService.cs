465bd3cff9f160155469b0bd9eb6e063e268e6b1
Vladislav Pomozov <vladuk2332@gmail.com>
2026-02-02 12:48:53 +0300

    removed default value for cancellation Token in interfaces

--- Vladify/Vladify.BusinessLogic/ServiceInterfaces/IUserService.cs
+++ Vladify/Vladify.BusinessLogic/ServiceInterfaces/IUserService.cs
@@ -4,9 +4,9 @@ namespace Vladify.BusinessLogic.ServiceInterfaces;
 
 public interface IUserService
 {
-    public Task<UserModel?> GetUserByIdAsync(Guid userId, bool isTracking, CancellationToken cancellationToken = default);
-    public Task<IEnumerable<UserModel>> GetUsersAsync(PaginationFilter filter, CancellationToken cancellationToken = default);
-    public Task<UserModel> AddUserAsync(UserRequestModel userRequestModel, CancellationToken cancellationToken = default);
-    public Task<UserModel> UpdateUserAsync(UserUpdateRequestModel userModel, CancellationToken cancellationToken = default);
-    public Task DeleteUserAsync(Guid userId, CancellationToken cancellationToken = default);
+    public Task<UserModel?> GetUserByIdAsync(Guid userId, bool isTracking, CancellationToken cancellationToken);
+    public Task<IEnumerable<UserModel>> GetUsersAsync(PaginationFilter filter, CancellationToken cancellationToken);
+    public Task<UserModel> AddUserAsync(UserRequestModel userRequestModel, CancellationToken cancellationToken);
+    public Task<UserModel> UpdateUserAsync(UserUpdateRequestModel userModel, CancellationToken cancellationToken);
+    public Task DeleteUserAsync(Guid userId, CancellationToken cancellationToken);
 }
