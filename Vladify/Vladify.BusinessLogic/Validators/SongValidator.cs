7eb54c70eaf7a0109cc482f4716f28e2e8a32164
Vladislav Pomozov <vladuk2332@gmail.com>
2026-02-10 18:39:45 +0300

    edited validator for DRY purposes

--- Vladify/Vladify.BusinessLogic/Validators/SongValidator.cs
+++ Vladify/Vladify.BusinessLogic/Validators/SongValidator.cs
@@ -10,6 +10,12 @@ file static class Constraints
     public static readonly TimeSpan MinSongDuration = TimeSpan.FromSeconds(10);
 
     public static readonly TimeSpan MaxSongDuration = TimeSpan.FromMinutes(30);
+
+    public const string LengthExceededMessage = "The length of field '{PropertyName}' exceeds {MaxLength}!";
+
+    public const string FieldRequiredMessage = "Field '{PropertyName}' is required!";
+
+    public const string BetweenValueMessage = "{PropertyName} must be between {From} and {To}!";
 }
 
 public class SongValidator : AbstractValidator<SongRequestModel>
@@ -17,19 +23,19 @@ public class SongValidator : AbstractValidator<SongRequestModel>
     public SongValidator()
     {
         RuleFor(song => song.Title)
-            .NotEmpty().WithMessage("Field '{PropertyName}' is required!")
-            .MaximumLength(Constraints.MaxDefaultStringLength).WithMessage("The length of field '{PropertyName}' exceeds {MaxLength}!");
+            .NotEmpty().WithMessage(Constraints.FieldRequiredMessage)
+            .MaximumLength(Constraints.MaxDefaultStringLength).WithMessage(Constraints.LengthExceededMessage);
 
         RuleFor(song => song.Album)
-            .NotEmpty().WithMessage("Field '{PropertyName}' is required!")
-            .MaximumLength(Constraints.MaxDefaultStringLength).WithMessage("The length of field '{PropertyName}' exceeds {MaxLength}!");
+            .NotEmpty().WithMessage(Constraints.FieldRequiredMessage)
+            .MaximumLength(Constraints.MaxDefaultStringLength).WithMessage(Constraints.LengthExceededMessage);
 
         RuleFor(song => song.Author)
-            .NotEmpty().WithMessage("Field '{PropertyName}' is required!")
-            .MaximumLength(Constraints.MaxDefaultStringLength).WithMessage("The length of field '{PropertyName}' exceeds {MaxLength}!");
+            .NotEmpty().WithMessage(Constraints.FieldRequiredMessage)
+            .MaximumLength(Constraints.MaxDefaultStringLength).WithMessage(Constraints.LengthExceededMessage);
 
         RuleFor(song => song.Duration)
-            .NotEmpty().WithMessage("Field '{PropertyName}' is required!")
-            .InclusiveBetween(Constraints.MinSongDuration, Constraints.MaxSongDuration).WithMessage("{PropertyName} must be between {From} and {To}!");
+            .NotEmpty().WithMessage(Constraints.FieldRequiredMessage)
+            .InclusiveBetween(Constraints.MinSongDuration, Constraints.MaxSongDuration).WithMessage(Constraints.BetweenValueMessage);
     }
 }
