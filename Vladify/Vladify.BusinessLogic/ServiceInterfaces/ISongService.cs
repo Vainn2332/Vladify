2ac983e3c9b566c2c6d2e077e8a224fb6cb94d90
Vladislav Pomozov <vladuk2332@gmail.com>
2026-02-20 16:02:24 +0300

    minor fix

--- Vladify/Vladify.BusinessLogic/ServiceInterfaces/ISongService.cs
+++ Vladify/Vladify.BusinessLogic/ServiceInterfaces/ISongService.cs
@@ -8,6 +8,6 @@ public interface ISongService
     public Task<SongModel?> GetSongByIdAsync(Guid songId, bool isTracking, CancellationToken cancellationToken);
     public Task<IEnumerable<SongModel>> GetSongsAsync(PaginationFilter filter, CancellationToken cancellationToken);
     public Task<SongModel> AddSongAsync(SongRequestModel songRequestModel, CancellationToken cancellationToken);
-    public Task<SongModel> UpdateSongAsync(SongModel SongModel, CancellationToken cancellationToken);
+    public Task<SongModel> UpdateSongAsync(SongModel songModel, CancellationToken cancellationToken);
     public Task DeleteSongAsync(Guid songId, CancellationToken cancellationToken);
 }
