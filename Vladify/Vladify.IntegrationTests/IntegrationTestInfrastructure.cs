e88d227f7f5f2e5bf743327b29bed2adacc177f5
Vladislav Pomozov <vladuk2332@gmail.com>
2026-02-25 16:23:31 +0300

    added testInfrastructure

---
+++ Vladify/Vladify.IntegrationTests/IntegrationTestInfrastructure.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Testcontainers.MsSql;
+using Vladify.DataAccess;
+
+namespace Vladify.IntegrationTests;
+
+public class IntegrationTestInfrastructure : IAsyncLifetime
+{
+    private readonly MsSqlContainer _testDbContainer = new MsSqlBuilder().Build();
+
+    public WebApplicationFactory<Program> Factory { get; private set; } = null!;
+
+    public HttpClient Client { get; private set; } = null!;
+
+    public async Task InitializeAsync()
+    {
+        await _testDbContainer.StartAsync();
+        Factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll<DbContextOptions<ApplicationDbContext>>();
+
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(_testDbContainer.GetConnectionString()));
+
+                services.Con
+            });
+        });
+
+        Client = Factory.CreateClient();
+    }
+
+    public async Task DisposeAsync()
+    {
+        Client.Dispose();
+        await Factory.DisposeAsync();
+        await _testDbContainer.DisposeAsync();
+    }
+    public static string GenerateTestJWT()
+    {
+        var claims = new List<Claim>();
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(TestConstants.TestSecretKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: TestConstants.Issuer,
+            audience: TestConstants.Audience,
+            expires: DateTime.Now.AddMinutes(5),
+            signingCredentials: credentials
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
---
+++ Vladify/Vladify.IntegrationTests/TestConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace Vladify.IntegrationTests;
+
+public static class TestConstants
+{
+    public const string Songs = "/api/songs";
+
+    public const string Users = "/api/users";
+
+    public const string TestSecretKey = "superSecretTestKeyThatIsIntendedOnlyForTestPurposes!";
+
+    public const string Issuer = "testIssuer";
+
+    public const string Audience = "testAudience";
+}
