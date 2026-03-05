01d6c35f421a8692d1c78c60a1bdcd68d86decd9
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-05 16:29:43 +0300

    added auth0Service

---
+++ Vladify/Vladify.BusinessLogic/Auth0Service.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Vladify.BusinessLogic.ServiceInterfaces;
+using Vladify.Options;
+namespace Vladify.BusinessLogic;
+
+public class Auth0Service(IOptions<Auth0Options> _options) : IAuth0Service
+{
+    private readonly Auth0Options _authOptions = _options.Value;
+    public async Task DeleteUserAsync(string authId)
+    {
+        var token = await GetManagementTokenAsync();
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        using var response = await httpClient.DeleteAsync($"https://{_authOptions.Domain}/api/v2/users/{authId}");
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task<string> GetManagementTokenAsync()
+    {
+        var httpClient = new HttpClient();
+
+        using var response = await httpClient.PostAsJsonAsync($"https://{_options.Value.Domain}/oauth/token", new
+        {
+            client_id = _authOptions.ClientId,
+            client_secret = _authOptions.ClientSecret,
+            audience = $"https://{_authOptions.Domain}/api/v2/",
+            grant_type = "client_credentials"
+        });
+
+        var token = await response.Content.ReadFromJsonAsync<string>();
+        return token!;
+    }
+}
---
+++ Vladify/Vladify.BusinessLogic/ServiceInterfaces/IAuth0Service.cs
@@ -0,0 +1,7 @@
+﻿namespace Vladify.BusinessLogic.ServiceInterfaces;
+
+public interface IAuth0Service
+{
+    public Task<string> GetManagementTokenAsync();
+    public Task DeleteUserAsync(string authId);
+}
