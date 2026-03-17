ca38c616407f138e8ef3da31a54dff89abb1a204
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-17 16:57:59 +0300

    added clientCredentials

---
+++ Vladify/Vladify.BusinessLogic/Options/ClientCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace Vladify.BusinessLogic.Options;
+
+public class ClientCredentials
+{
+    public required string ClientId { get; set; }
+
+    public required string ClientSecret { get; set; }
+}
