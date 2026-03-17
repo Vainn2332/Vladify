7d05cfe9ba6ad0b88afaae69a690db078bdbca80
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-17 16:21:57 +0300

    updated AuthOptions

--- Vladify/Vladify.BusinessLogic/Options/Auth0Options.cs
+++ Vladify/Vladify.BusinessLogic/Options/Auth0Options.cs
@@ -1,11 +1,15 @@
-﻿namespace Vladify.Options;
+﻿using Vladify.BusinessLogic.Options;
+
+namespace Vladify.Options;
 
 public class Auth0Options
 {
     public const string SectionName = "Auth0";
 
     public required string Domain { get; set; }
-    public required string ClientId { get; set; }
     public required string Audience { get; set; }
-    public required string ClientSecret { get; set; }
+
+    public required ClientCredentials PublicClient { get; set; }
+
+    public required ClientCredentials M2MClient { get; set; }
 }
