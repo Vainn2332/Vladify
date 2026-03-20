3f9d5a860b652763ba345551abf935f443e6b81b
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-20 15:31:36 +0300

    added api key checking action filter

---
+++ Vladify/Vladify/Filters/ApiKeyFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using Vladify.BusinessLogic.Exceptions;
+using Vladify.Options;
+
+namespace Vladify.Filters;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+public class ApiKeyFilter(string apiKeyName) : Attribute, IAsyncActionFilter
+{
+    private const string HeaderName = "X-Api-Key";
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var options = context.HttpContext.RequestServices.GetRequiredService<IOptions<ApiKeysOptions>>();
+
+        var property = typeof(ApiKeysOptions).GetProperty(apiKeyName)
+            ?? throw new NotFoundException($"property{apiKeyName} is not found");
+
+        var apiKey = property.GetValue(options)?.ToString();
+
+        if (!context.HttpContext.Request.Headers.TryGetValue(HeaderName, out var extractedApiKey) ||
+            apiKey != extractedApiKey)
+        {
+            throw new UnauthorizedException("Invalid ApiKey!");
+        }
+
+        await next();
+    }
+}
