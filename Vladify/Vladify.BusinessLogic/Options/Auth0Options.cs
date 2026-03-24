cdfb3711a832af2730185ab8bbd7ef9142e9dfba
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-24 16:59:36 +0300

    edited Auth0 config

--- Vladify/Vladify.BusinessLogic/Options/Auth0Options.cs
+++ Vladify/Vladify.BusinessLogic/Options/Auth0Options.cs
@@ -2,20 +2,11 @@
 
 public class Auth0Options
 {
-    private const string TokenEndpointPath = "oauth/token";
-    private const string ManagementApiPath = "api/v2/";
-
     public const string SectionName = "Auth0";
 
     public required string Domain { get; set; }
 
-    public required string Audience { get; set; }
+    public required string TokenUrl { get; set; }
 
-    public required ClientCredentials PublicClient { get; set; }
-
-    public required ClientCredentials M2MClient { get; set; }
-
-    public string TokenUrl => $"https://{Domain}/{TokenEndpointPath}";
-
-    public string ManagementApiAudience => $"https://{Domain}/{ManagementApiPath}";
+    public required ClientCredentials ClientCredentials { get; set; }
 }
--- Vladify/Vladify.BusinessLogic/Options/ClientCredentials.cs
+++ Vladify/Vladify.BusinessLogic/Options/ClientCredentials.cs
@@ -5,4 +5,6 @@ public class ClientCredentials
     public required string ClientId { get; set; }
 
     public required string ClientSecret { get; set; }
+
+    public required string Audience { get; set; }
 }
