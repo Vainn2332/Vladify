bf2cb0cb0b7be729d5a8c529aedc1fbd5c8966d3
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-24 22:00:26 +0300

    Revert "edited Auth0 config"
    This reverts commit cdfb3711a832af2730185ab8bbd7ef9142e9dfba.

--- Vladify/Vladify.BusinessLogic/Options/Auth0Options.cs
+++ Vladify/Vladify.BusinessLogic/Options/Auth0Options.cs
@@ -2,11 +2,20 @@
 
 public class Auth0Options
 {
+    private const string TokenEndpointPath = "oauth/token";
+    private const string ManagementApiPath = "api/v2/";
+
     public const string SectionName = "Auth0";
 
     public required string Domain { get; set; }
 
-    public required string TokenUrl { get; set; }
+    public required string Audience { get; set; }
 
-    public required ClientCredentials ClientCredentials { get; set; }
+    public required ClientCredentials PublicClient { get; set; }
+
+    public required ClientCredentials M2MClient { get; set; }
+
+    public string TokenUrl => $"https://{Domain}/{TokenEndpointPath}";
+
+    public string ManagementApiAudience => $"https://{Domain}/{ManagementApiPath}";
 }
--- Vladify/Vladify.BusinessLogic/Options/ClientCredentials.cs
+++ Vladify/Vladify.BusinessLogic/Options/ClientCredentials.cs
@@ -5,6 +5,4 @@ public class ClientCredentials
     public required string ClientId { get; set; }
 
     public required string ClientSecret { get; set; }
-
-    public required string Audience { get; set; }
 }
