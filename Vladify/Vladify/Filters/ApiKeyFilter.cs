bfcf95dc0bec0f23c30c990693f98d81fcf7f3a2
Vladislav Pomozov <vladuk2332@gmail.com>
2026-03-24 15:32:48 +0300

    updated ApiKeyFilter

--- Vladify/Vladify/Filters/ApiKeyFilter.cs
+++ Vladify/Vladify/Filters/ApiKeyFilter.cs
@@ -1,35 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Options;
-using System.Collections.Concurrent;
 using Vladify.BusinessLogic.Exceptions;
-using Vladify.Options;
+using Vladify.BusinessLogic.Options;
 
 namespace Vladify.Filters;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
-public class ApiKeyFilter(string apiKeyName) : Attribute, IAsyncActionFilter
+public class ApiKeyFilter(string apiKeyName) : Attribute, IActionFilter
 {
     private const string HeaderName = "X-Api-Key";
-    private static readonly ConcurrentDictionary<string, string> _cache = new();
 
-    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    public void OnActionExecuting(ActionExecutingContext context)
     {
-        var options = context.HttpContext.RequestServices.GetRequiredService<IOptions<ApiKeysOptions>>();
-
-        var apiKey = _cache.GetOrAdd(apiKeyName, apiKeyValue =>
-        {
-            var propertyInfo = typeof(ApiKeysOptions).GetProperty(apiKeyName)
-                ?? throw new NotFoundException($"property {apiKeyName} is not found!");
-            return propertyInfo.GetValue(options.Value)?.ToString()!;
-        });
-
+        var optionsMonitor = context.HttpContext.RequestServices.GetRequiredService<IOptionsMonitor<ApiKeysOptions>>();
+        var apiKey = optionsMonitor.Get(apiKeyName).Value;
 
         if (!context.HttpContext.Request.Headers.TryGetValue(HeaderName, out var extractedApiKey) ||
             apiKey != extractedApiKey)
         {
             throw new UnauthorizedException("Invalid ApiKey!");
         }
+    }
 
-        await next();
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
     }
 }
