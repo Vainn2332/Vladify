d749341a3ecd2d4494b5e3d46c3e4677f2c3c693
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-28 09:57:47 +0300

    add playlist entity

---
+++ Vladify/Vladify.DataAccess/Entities/Playlist.cs
@@ -0,0 +1,17 @@
+﻿using Vladify.DataAccess.Interfaces;
+
+namespace Vladify.DataAccess.Entities;
+
+public class Playlist : IEntity
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public required string Name { get; set; }
+
+    public required User User { get; set; }
+
+    public required ICollection<Song> Songs { get; set; }
+
+}
