08b8f396b653e11f55b763f2454c25e213fe6ab9
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-28 09:58:22 +0300

    add anavigation properties

--- Vladify/Vladify.DataAccess/Entities/Song.cs
+++ Vladify/Vladify.DataAccess/Entities/Song.cs
@@ -10,7 +10,11 @@ public class Song : IEntity
 
     public string Album { get; set; } = null!;
 
-    public string Author { get; set; } = null!;
+    public Guid AuthorId { get; set; }
 
     public TimeSpan Duration { get; set; } = TimeSpan.Zero;
+
+    public required User User { get; set; }
+
+    public required ICollection<Playlist> Playlists { get; set; } = [];
 }
