8f217d64344fd440aa933c66923c3c797c14b9f8
Vladislav Pomozov <vladuk2332@gmail.com>
2026-04-29 21:24:46 +0300

    add songRepo

---
+++ Vladify/Vladify.DataAccess/Repositories/SongRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vladify.DataAccess.Entities;
+using Vladify.DataAccess.Interfaces;
+
+namespace Vladify.DataAccess.Repositories;
+
+public class SongRepository(ApplicationDbContext context) : Repository<Song>(context), ISongRepository
+{
+    public Task<Song?> GetSongWithUserInfoByIdAsync(Guid id, bool isTracking, CancellationToken cancellationToken)
+    {
+        return (isTracking ? _context.Songs : _context.Songs.AsNoTracking())
+            .Include(p => p.Owner)
+            .FirstOrDefaultAsync(u => u.Id == id, cancellationToken);
+    }
+
+    public async Task<IEnumerable<Song>> GetSongsWithUserInfoByIdAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
+    {
+        return await _context.Songs
+            .Include(p => p.Owner)
+            .OrderBy(p => p.Id)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+    }
+}
