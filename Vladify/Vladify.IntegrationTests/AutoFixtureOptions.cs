c0635efd0ad59fdc46de162451b4389f50fba188
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-02 11:05:28 +0300

    add unitTests

--- Vladify/Vladify.IntegrationTests/AutoFixtureOptions.cs
+++ Vladify/Vladify.IntegrationTests/AutoFixtureOptions.cs
@@ -29,6 +29,11 @@ public static class AutoFixtureOptions
         .With(s => s.Playlists, () => null!)
         );
 
+        fixture.Customize<Playlist>(builder => builder
+        .With(s => s.Owner, () => null!)
+        .With(s => s.Songs, () => null!)
+        );
+
         return fixture;
     }
 }
