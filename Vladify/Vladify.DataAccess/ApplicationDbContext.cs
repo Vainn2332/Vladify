d836b64d7809e6f5dbdb1f2763c2fe2c6b507287
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-06 14:48:29 +0300

    update seeding

--- Vladify/Vladify.DataAccess/ApplicationDbContext.cs
+++ Vladify/Vladify.DataAccess/ApplicationDbContext.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Vladify.DataAccess.Constants;
 using Vladify.DataAccess.DbConfig;
 using Vladify.DataAccess.Entities;
+using Vladify.DataAccess.Fakers;
 
 namespace Vladify.DataAccess;
 
@@ -14,6 +17,21 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
         modelBuilder.ApplyConfiguration(new UserDbConfig());
         modelBuilder.ApplyConfiguration(new SongDbConfig());
 
+        SeedData(modelBuilder);
+
         base.OnModelCreating(modelBuilder);
+    }
+
+    private static void SeedData(ModelBuilder modelBuilder)
+    {
+        Randomizer.Seed = new Random(DataAccessLayerConstants.RandomSeedDataNumber);
+
+        var users = new UserFaker().Generate(DataAccessLayerConstants.UserSeedDataAmount);
+        var userIds = users.Select(u => u.Id).ToList();
+
+        var songs = new SongFaker(userIds).Generate(DataAccessLayerConstants.SongSeedDataAmount);
+
+        modelBuilder.Entity<User>().HasData(users);
+        modelBuilder.Entity<Song>().HasData(songs);
     }
 }
