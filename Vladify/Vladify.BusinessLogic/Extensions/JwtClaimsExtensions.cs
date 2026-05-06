2632bd759fadd2cf7ec2bf1839a9b08da86f4602
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-06 15:15:55 +0300

    add jwtClaimExtension

---
+++ Vladify/Vladify.BusinessLogic/Extensions/JwtClaimsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Vladify.BusinessLogic.Constants;
+using Vladify.BusinessLogic.Exceptions;
+
+namespace Vladify.BusinessLogic.Extensions;
+
+public static class JwtClaimsExtensions
+{
+    public static string GetEmail(this ClaimsPrincipal principal)
+    {
+        var userEmail = principal.FindFirst(BusinessLogicLayerConstants.CustomEmailClaimName)?.Value
+            ?? throw new UnauthorizedException("unable to get user externalId");
+
+        return userEmail;
+    }
+}
