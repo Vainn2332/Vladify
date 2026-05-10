6a25d11ac60aee46db4d483c67b78b0ab8e00a15
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 19:31:27 +0300

    add controller

---
+++ Vladify/Vladify/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Vladify.BusinessLogic.Models;
+using Vladify.BusinessLogic.ServiceInterfaces;
+
+namespace Vladify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SearchController(ISearchService searchService) : ControllerBase
+    {
+        [HttpGet]
+        public Task<SearchResultDto> SearchAsync([FromQuery] string query, CancellationToken cancellationToken)
+        {
+            return searchService.SearchAsync(query, cancellationToken);
+        }
+    }
+}
