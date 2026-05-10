d9601fe11be4e0992350a1c675dbb2266eeb6326
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 19:26:28 +0300

    add search repository

---
+++ Vladify/Vladify.DataAccess/Interfaces/ISearchRepository.cs
@@ -0,0 +1,8 @@
+﻿using Vladify.DataAccess.Entities;
+
+namespace Vladify.DataAccess.Interfaces;
+
+public interface ISearchRepository
+{
+    Task<(ICollection<Song>, ICollection<User>)> SearchAsync(string query, CancellationToken cancellationToken);
+}
---
+++ Vladify/Vladify.DataAccess/Repositories/SearchRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vladify.DataAccess.Constants;
+using Vladify.DataAccess.Entities;
+using Vladify.DataAccess.Interfaces;
+
+namespace Vladify.DataAccess.Repositories;
+
+internal class SearchRepository(ApplicationDbContext context) : ISearchRepository
+{
+    public async Task<(ICollection<Song>, ICollection<User>)> SearchAsync(string query, CancellationToken cancellationToken)
+    {
+        var originalQuery = query.ToLower();
+
+        var songs = await context.Songs
+            .Include(p => p.Owner)
+            .Where(p => p.Title.ToLower().Contains(originalQuery))
+            .Take(DataAccessLayerConstants.SongSearchAmount)
+            .ToListAsync(cancellationToken);
+
+        var users = await context.Users
+            .Where(p => p.Name.ToLower().Contains(originalQuery))
+            .Take(DataAccessLayerConstants.UserSearchAmount)
+            .ToListAsync(cancellationToken);
+
+        return (songs, users);
+    }
+}
