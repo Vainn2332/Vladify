d55e7ee4c12651069ecd770cc56bec3c6357034f
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 19:27:01 +0300

    add model

---
+++ Vladify/Vladify.BusinessLogic/Models/SearchResultDto.cs
@@ -0,0 +1,10 @@
+﻿using Vladify.BusinessLogic.Models.SongModels;
+using Vladify.BusinessLogic.Models.UserModels;
+
+namespace Vladify.BusinessLogic.Models;
+
+public class SearchResultDto
+{
+    public ICollection<SongModel> Songs { get; set; } = [];
+    public ICollection<UserModel> Users { get; set; } = [];
+}
