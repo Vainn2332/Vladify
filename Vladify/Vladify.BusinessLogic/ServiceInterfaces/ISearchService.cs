2c3a0c99a235b06bb302f4f44cb31299d4950c68
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-10 19:26:56 +0300

    add service

---
+++ Vladify/Vladify.BusinessLogic/ServiceInterfaces/ISearchService.cs
@@ -0,0 +1,8 @@
+﻿using Vladify.BusinessLogic.Models;
+
+namespace Vladify.BusinessLogic.ServiceInterfaces;
+
+public interface ISearchService
+{
+    public Task<SearchResultDto> SearchAsync(string query, CancellationToken cancellationToken);
+}
---
+++ Vladify/Vladify.BusinessLogic/Services/SearchService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Vladify.BusinessLogic.Models;
+using Vladify.BusinessLogic.Models.SongModels;
+using Vladify.BusinessLogic.Models.UserModels;
+using Vladify.BusinessLogic.ServiceInterfaces;
+using Vladify.DataAccess.Interfaces;
+
+namespace Vladify.BusinessLogic.Services;
+
+public class SearchService(ISearchRepository searchRepository, IMapper mapper) : ISearchService
+{
+    public async Task<SearchResultDto> SearchAsync(string query, CancellationToken cancellationToken)
+    {
+        var (songs, users) = await searchRepository.SearchAsync(query, cancellationToken);
+
+        var songModels = mapper.Map<ICollection<SongModel>>(songs);
+        var userModels = mapper.Map<ICollection<UserModel>>(users);
+
+        var searchResult = new SearchResultDto()
+        {
+            Songs = songModels,
+            Users = userModels
+        };
+
+        return searchResult;
+    }
+}
