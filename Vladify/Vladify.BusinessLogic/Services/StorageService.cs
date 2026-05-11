c8bffc0e205122198c56e11520fce1c3f2b4b1e7
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-11 20:54:58 +0300

    update storageService

--- Vladify/Vladify.BusinessLogic/Services/StorageService.cs
+++ Vladify/Vladify.BusinessLogic/Services/StorageService.cs
@@ -7,34 +7,72 @@ using Vladify.BusinessLogic.ServiceInterfaces;
 
 namespace Vladify.BusinessLogic.Services;
 
+file static class Constraints
+{
+    public static readonly string[] allowedAudioExtensions = new[] { ".mp3", ".wav", ".flac" };
+
+    public static readonly string[] allowedImageExtensions = new[] { ".jpg", ".png", ".jpeg" };
+}
+
 public class StorageService(IAmazonS3 amazonS3Client, IOptions<MinioOptions> options) : IStorageService
 {
     public async Task<(string audioUrl, string imageUrl)> UploadAsync(IFormFile audioFile, IFormFile imageFile, CancellationToken cancellationToken)
     {
-        var audioUrl = await UploadAsync(audioFile, options.Value.MusicBucketName, cancellationToken);
-        var imageUrl = await UploadAsync(imageFile, options.Value.MusicIconBucketName, cancellationToken);
+        var audioUrl = await UploadAudioAsync(audioFile, cancellationToken);
+        var imageUrl = await UploadImageAsync(imageFile, cancellationToken);
 
         return (audioUrl, imageUrl);
     }
 
-    private async Task<string> UploadAsync(IFormFile file, string bucketName, CancellationToken cancellationToken)
+    private async Task<string> UploadAudioAsync(IFormFile audioFile, CancellationToken cancellationToken)
     {
-        var extension = Path.GetExtension(file.FileName).ToLower();
+        var extension = Path.GetExtension(audioFile.FileName).ToLower();
+        if (!Constraints.allowedAudioExtensions.Contains(extension))
+        {
+            throw new ArgumentException("invalid format of audioFile!");
+        }
+
         var uniqueFileName = $"{Guid.NewGuid()}{extension}";
 
-        using var stream = file.OpenReadStream();
+        using var stream = audioFile.OpenReadStream();
 
         var putRequest = new PutObjectRequest
         {
-            BucketName = bucketName,
+            BucketName = options.Value.MusicBucketName,
             Key = uniqueFileName,
             InputStream = stream,
-            ContentType = file.ContentType,
+            ContentType = audioFile.ContentType,
             DisablePayloadSigning = true
         };
 
         await amazonS3Client.PutObjectAsync(putRequest, cancellationToken);
 
-        return $"{options.Value.ServiceUrl}/{bucketName}/{uniqueFileName}";
+        return $"{options.Value.ServiceUrl}/{options.Value.MusicBucketName}/{uniqueFileName}";
+    }
+
+    private async Task<string> UploadImageAsync(IFormFile imageFile, CancellationToken cancellationToken)
+    {
+        var extension = Path.GetExtension(imageFile.FileName).ToLower();
+        if (!Constraints.allowedImageExtensions.Contains(extension))
+        {
+            throw new ArgumentException("invalid format of imageFile!");
+        }
+
+        var uniqueFileName = $"{Guid.NewGuid()}{extension}";
+
+        using var stream = imageFile.OpenReadStream();
+
+        var putRequest = new PutObjectRequest
+        {
+            BucketName = options.Value.MusicIconBucketName,
+            Key = uniqueFileName,
+            InputStream = stream,
+            ContentType = imageFile.ContentType,
+            DisablePayloadSigning = true
+        };
+
+        await amazonS3Client.PutObjectAsync(putRequest, cancellationToken);
+
+        return $"{options.Value.ServiceUrl}/{options.Value.MusicIconBucketName}/{uniqueFileName}";
     }
 }
