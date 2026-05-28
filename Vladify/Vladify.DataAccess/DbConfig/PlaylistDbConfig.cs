9bfac3b3c2b588a0e450a7a5503a43495814291c
Vladislav Pomozov <vladuk2332@gmail.com>
2026-05-28 14:48:52 +0300

    create playlistDbConfig

---
+++ Vladify/Vladify.DataAccess/DbConfig/PlaylistDbConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vladify.DataAccess.Constants;
+using Vladify.DataAccess.Entities;
+
+namespace Vladify.DataAccess.DbConfig;
+
+public class PlaylistDbConfig : IEntityTypeConfiguration<Playlist>
+{
+    public void Configure(EntityTypeBuilder<Playlist> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Name).HasMaxLength(DataAccessLayerConstants.MaxStandartStringLength);
+
+        builder.HasMany(p => p.Songs)
+            .WithMany(p => p.Playlists);
+    }
+}
