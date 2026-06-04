7814769d99842ce291397bbc9ba70f3d13bb00be
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-04 12:56:12 +0300

    add models

---
+++ Vladify/Vladify.BusinessLogic/Models/PlaylistModels/PlaylistModel.cs
@@ -0,0 +1,14 @@
+﻿using Vladify.BusinessLogic.Models.SongModels;
+
+namespace Vladify.BusinessLogic.Models.PlaylistModels;
+
+public class PlaylistModel
+{
+    public Guid Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string AuthorName { get; set; }
+
+    public required IEnumerable<SongModel> Songs { get; set; }
+}
