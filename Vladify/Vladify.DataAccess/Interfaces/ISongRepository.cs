5844ff0c75b5668e4ace60ec2b0206ad09101cb5
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-15 11:49:54 +0300

    update interface

--- Vladify/Vladify.DataAccess/Interfaces/ISongRepository.cs
+++ Vladify/Vladify.DataAccess/Interfaces/ISongRepository.cs
@@ -9,5 +9,4 @@ public interface ISongRepository : IRepository<Song>
     public Task<Song> UpdateSongAsync(Song song, CancellationToken cancellationToken);
     public Task<Song?> GetSongWithUserInfoByIdAsync(Guid id, bool isTracking, CancellationToken cancellationToken);
     public Task<IEnumerable<Song>> GetSongsWithUserInfoByIdAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
-    public Task<IEnumerable<Song>> GetRecentlyAddedSongsAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
 }
