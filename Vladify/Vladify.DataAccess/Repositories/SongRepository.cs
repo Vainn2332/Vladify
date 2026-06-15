a18b9dcefd8067c0ae5802056f6a377c055e9e55
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-15 12:37:08 +0300

    add songRepo

---
+++ Vladify/Vladify.DataAccess/Repositories/SongRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vladify.DataAccess.Entities;
+using Vladify.DataAccess.Interfaces;
+
+namespace Vladify.DataAccess.Repositories;
+
+public class SongRepository(ApplicationDbContext context) : Repository<Song>(context), ISongRepository
+{
+    public override async Task<Song> AddAsync(Song song, CancellationToken cancellationToken)
+    {
+        await _context.Songs.AddAsync(song, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        await _context.Entry(song).Reference(p => p.Owner).LoadAsync(cancellationToken);
+
+        return song;
+    }
+
+    public override Task<Song?> GetByIdAsync(Guid id, bool isTracking, CancellationToken cancellationToken)
+    {
+        return (isTracking ? _context.Songs : _context.Songs.AsNoTracking())
+            .Include(p => p.Owner)
+            .FirstOrDefaultAsync(u => u.Id == id, cancellationToken);
+    }
+
+    public override async Task<IEnumerable<Song>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
+    {
+        return await _context.Songs
+            .Include(p => p.Owner)
+            .OrderBy(p => p.Id)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+    }
+
+    public override async Task<Song> UpdateAsync(Song song, CancellationToken cancellationToken)
+    {
+        _context.Songs.Update(song);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        await _context.Entry(song).Reference(p => p.Owner).LoadAsync(cancellationToken);
+
+        return song;
+    }
+}
