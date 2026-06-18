841bb454a3ea4dd0edb9a8937f4a5cc47466d75d
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-18 15:22:33 +0300

    Reapply "Merge branch 'main' into feature/add-mass-transit"
    This reverts commit 1853e94d6753a9bf039db73bb3cd23bdcd538966.

---
+++ Vladify/Vladify.DataAccess/Interfaces/ISongRepository.cs
@@ -0,0 +1,6 @@
+﻿using Vladify.DataAccess.Entities;
+
+namespace Vladify.DataAccess.Interfaces;
+
+// temporarily empty for future specialized logic
+public interface ISongRepository : IRepository<Song> { }
