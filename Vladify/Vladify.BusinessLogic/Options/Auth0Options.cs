e31ed1dd90dd9032d40752c2b6773507cbbc5d1f
Vladislav Pomozov <vladuk2332@gmail.com>
2026-06-18 12:54:04 +0300

    add required attribute to options

--- Vladify/Vladify.BusinessLogic/Options/Auth0Options.cs
+++ Vladify/Vladify.BusinessLogic/Options/Auth0Options.cs
@@ -1,14 +1,20 @@
-﻿namespace Vladify.BusinessLogic.Options;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vladify.BusinessLogic.Options;
 
 public class Auth0Options
 {
     public const string SectionName = "Auth0";
 
+    [Required]
     public required string Domain { get; set; }
 
+    [Required]
     public required string TokenUrl { get; set; }
 
+    [Required]
     public required ClientCredentials PublicClient { get; set; }
 
+    [Required]
     public required ClientCredentials M2MClient { get; set; }
 }
