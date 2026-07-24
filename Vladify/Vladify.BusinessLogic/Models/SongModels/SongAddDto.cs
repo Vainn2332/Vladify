bc5d3ba548bbc2c6479f78d6839c564c72e83efb
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-24 14:33:51 +0200

    add songAddDto

---
+++ Vladify/Vladify.BusinessLogic/Models/SongModels/SongAddDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Vladify.BusinessLogic.Models.SongModels;
+
+public class SongAddDto
+{
+    public required string Title { get; set; }
+
+    public required string Album { get; set; }
+
+    public required TimeSpan Duration { get; set; }
+
+    public Guid AuthorId { get; set; }
+}
