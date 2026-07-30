a446e7ab40cd1a6ac0f31d17493690b21574dd81
Vladislav Pomozov <vladuk2332@gmail.com>
2026-07-30 15:56:27 +0200

    add client

---
+++ Vladify/Vladify.DataAccess/Interfaces/IModerationIntegrationClient.cs
@@ -0,0 +1,8 @@
+﻿using Vladify.DataAccess.Dtos;
+
+namespace Vladify.DataAccess.Interfaces;
+
+public interface IModerationIntegrationClient
+{
+    public Task<ModerationTaskDto> CreateTaskAsync(string songId, CancellationToken cancellationToken);
+}
