2a99976adc08bcef8dd1cf122b96d65b21e94003
Vladislav Pomozov <vladuk2332@gmail.com>
2026-08-28 16:57:51 +0300

    Feat/add grpc (#45)
    * add proto file
    
    * add dto
    
    * add client
    
    * add url to appsettings
    
    * update service
    
    * update logic
    
    * add to di
    
    * update appsettings
    
    * update tests

---
+++ Vladify/Vladify.DataAccess/Clients/ModerationIntegrationClient.cs
@@ -0,0 +1,24 @@
+﻿using Vladify.DataAccess.Dtos;
+using Vladify.DataAccess.Interfaces;
+using Vladify.GrpcContracts;
+
+namespace Vladify.DataAccess.Clients;
+
+public class ModerationIntegrationClient(ModerationGrpc.ModerationGrpcClient client) : IModerationIntegrationClient
+{
+    public async Task<ModerationTaskDto> CreateTaskAsync(string songId, CancellationToken cancellationToken)
+    {
+        var request = new CreateTaskRequest()
+        {
+            SongId = songId
+        };
+
+        var response = await client.CreateTaskAsync(request, cancellationToken: cancellationToken);
+        if (!Guid.TryParse(response.TaskId, out var id))
+        {
+            throw new ArgumentException("Invalid Id!");
+        }
+
+        return new ModerationTaskDto() { Id = id };
+    }
+}
---
+++ Vladify/Vladify.DataAccess/Dtos/ModerationTaskDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Vladify.DataAccess.Dtos;
+
+public class ModerationTaskDto
+{
+    public Guid Id { get; set; }
+}
