89313e70d4dcca0dba708523a0fb80e17ce8e668
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-06 12:58:34 +0300

    add cors options

---
+++ Vladify/Vladify.BusinessLogic/Options/CorsOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Vladify.BusinessLogic.Options;
+
+public class CorsOptions
+{
+    public const string SectionName = "Cors";
+
+    public required string[] AllowedOrigins { get; set; }
+}
