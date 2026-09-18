437557c4868cd0c07080a161875098696ec930c3
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-18 14:21:19 +0300

    test

---
+++ Vladify/Vladify.BusinessLogic/Models/FileData.cs
@@ -0,0 +1,10 @@
+﻿namespace Vladify.BusinessLogic.Models;
+
+public class FileData
+{
+    public required Stream Content { get; set; }
+
+    public required string ContentType { get; set; }
+
+    public required string FileName { get; set; }
+}
