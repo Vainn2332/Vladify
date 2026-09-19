a3bf9137dbc20628026210edd3a52d2f521b0d8c
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-19 17:19:13 +0300

    update bootstrap constants

--- Vladify/Vladify.IntegrationTests/BootstrapConstants.cs
+++ Vladify/Vladify.IntegrationTests/BootstrapConstants.cs
@@ -6,4 +6,6 @@ internal static class BootstrapConstants
     public const string MinioPassword = "minioadmin";
     public const string TestBucket = "test-bucket";
 
+
+    public const string MinioImageContainerVersion = "quay.io/minio/minio:RELEASE.2025-09-07T16-13-09Z";
 }
