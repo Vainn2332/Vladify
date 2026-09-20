abf86950fb8e8f1db5465d4c26cda079f1df5479
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-20 13:23:43 +0300

    create StorageInspector class

---
+++ Vladify/Vladify.IntegrationTests/Infrastructure/StorageInspector.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.S3;
+using System.Net;
+using Vladify.IntegrationTests.Constants;
+
+namespace Vladify.IntegrationTests.Infrastructure;
+
+public static class StorageInspector
+{
+    public static async Task<bool> CheckPresenceInBucket(IAmazonS3 s3, string url, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var data = await s3.GetObjectMetadataAsync(BootstrapConstants.TestBucket, url, cancellationToken);
+
+            return true;
+        }
+        catch (AmazonS3Exception e) when (e.StatusCode == HttpStatusCode.NotFound)
+        {
+            return false;
+        }
+    }
+}
