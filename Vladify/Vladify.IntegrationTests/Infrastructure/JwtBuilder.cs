c46cbedd404b4051764158a18e85a7deb159b001
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-20 13:23:49 +0300

    create jwtBuilder

---
+++ Vladify/Vladify.IntegrationTests/Infrastructure/JwtBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Vladify.BusinessLogic.Constants;
+using Vladify.IntegrationTests.Constants;
+
+namespace Vladify.IntegrationTests.Infrastructure;
+
+public static class JwtBuilder
+{
+    public static string GenerateTestJWT(string userEmail = TestConstants.TestJwtEmailClaimValue)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(TestConstants.TestSecretKey));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var claims = new List<Claim>()
+        {
+            new Claim(ClaimConstants.CustomEmailClaimName, userEmail)
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: TestConstants.Issuer,
+            audience: TestConstants.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(5),
+            signingCredentials: credentials
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
