b3c334f8615731ff72667933930053e4d252c843
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-22 13:26:46 +0300

    add tests on pagination

--- Vladify/Vladify.IntegrationTests/Tests/PlaylistControllerTest.cs
+++ Vladify/Vladify.IntegrationTests/Tests/PlaylistControllerTest.cs
@@ -156,6 +156,58 @@ public class PlaylistsControllerTest
     }
 
     [Fact]
+    public async Task GetAllPlaylistsOfUser_Should_SetHasNextPageTrue_When_MoreItemsThanPage()
+    {
+        var user = _fixture.Create<User>();
+        var playlists = _fixture.CreateMany<Playlist>(3).ToList();
+        foreach (var playlist in playlists)
+            playlist.AuthorId = user.Id;
+        user.Playlists = playlists;
+
+        await _infrastructure.DataSeeder.SeedDataAsync(user);
+
+        var token = JwtBuilder.GenerateTestJWT();
+        _infrastructure.Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        using var response = await _infrastructure.Client.GetAsync($"{TestConstants.PlaylistsApiRoute}?PageNumber=1&PageSize=2");
+
+        await _infrastructure.DataResetter.ResetDataAsync();
+
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<PagedResponse<PlaylistModel>>();
+        result.Should().NotBeNull();
+        result.Data.Should().HaveCount(2);
+        result.HasNextPage.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task GetAllPlaylistsOfUser_Should_SetHasNextPageFalse_When_LastPageIsFull()
+    {
+        var user = _fixture.Create<User>();
+        var playlists = _fixture.CreateMany<Playlist>(2).ToList();
+        foreach (var playlist in playlists)
+            playlist.AuthorId = user.Id;
+        user.Playlists = playlists;
+
+        await _infrastructure.DataSeeder.SeedDataAsync(user);
+
+        var token = JwtBuilder.GenerateTestJWT();
+        _infrastructure.Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        using var response = await _infrastructure.Client.GetAsync($"{TestConstants.PlaylistsApiRoute}?PageNumber=1&PageSize=2");
+
+        await _infrastructure.DataResetter.ResetDataAsync();
+
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<PagedResponse<PlaylistModel>>();
+        result.Should().NotBeNull();
+        result.Data.Should().HaveCount(2);
+        result.HasNextPage.Should().BeFalse();
+    }
+
+    [Fact]
     public async Task UpdatePlaylist_Should_UpdateData_When_ValidInput()
     {
         var testUser = await _infrastructure.DataSeeder.SeedDataAsync(_fixture.Create<User>());
