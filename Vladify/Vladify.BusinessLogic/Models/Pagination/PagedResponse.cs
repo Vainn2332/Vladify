8255455f52cc6d3abf8ac98e32e0962f050d34fa
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-22 12:44:23 +0300

    create PagedResponse

---
+++ Vladify/Vladify.BusinessLogic/Models/Pagination/PagedResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Vladify.BusinessLogic.Models.Pagination;
+
+public class PagedResponse<TResponse>
+{
+    public required IReadOnlyCollection<TResponse> Data { get; set; }
+
+    public bool HasNextPage { get; set; }
+}
