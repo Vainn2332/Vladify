ba75c4ac55a2c913b5f4be707049aa6a52e3ee85
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-22 12:31:19 +0300

    create pagedResult

---
+++ Vladify/Vladify.DataAccess/Dtos/Pagination/PagedResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Vladify.DataAccess.Dtos.Pagination;
+
+public class PagedResult<T>
+{
+    public required ICollection<T> Data { get; set; }
+
+    public bool HasNextPage { get; set; }
+}
